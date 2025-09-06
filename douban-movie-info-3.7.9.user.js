ea6e820ba788e349ef83f97e610a1ff6571407b9
heartnn <457514+heartnn@users.noreply.github.com>
2025-09-06 08:25:02 +0800

    Update douban-movie-info-3.7.9.user.js

--- douban-movie-info-3.7.9.user.js
+++ douban-movie-info-3.7.9.user.js
@@ -4,7 +4,7 @@
 // @version            3.7.9
 // @author             Secant(TYT@NexusHD)
 // @include            http*://movie.douban.com/subject/*
-// @require            https://code.jquery.com/jquery-3.4.1.min.js
+// @require            https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js
 // @icon               https://movie.douban.com/favicon.ico
 // @contributionURL    https://i.loli.net/2020/02/28/JPGgHc3UMwXedhv.jpg
 // @contributionAmount 10
@@ -883,7 +883,7 @@
                       : f.name.chs
                     : f.name.for;
                   return (
-                    (i > 0 ? "　　　　　　" : "") +
+                    (i > 0 ? "　　　　  　" : "") +
                     name +
                     (f.character ? ` | ${f.character}` : "")
                   );
